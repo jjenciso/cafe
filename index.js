30e7e0511100f167e1bfb8c5315bc3866c5999aa
jerome-ElGwapo <jeromemain07@gmail.com>
2023-04-17 23:31:21 +0800

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const video = document.getElementById("background");
-const playButton = document.getElementById("playButton");
-const pauseButton = document.getElementById("pauseButton");
-const buttons = document.querySelectorAll("button");
+const playButton = document.querySelector(".play");
+const pauseButton = document.querySelector(".pause");
+const pausePlayButtons = document.querySelectorAll(".pausePlay");
 // CONTAINERS
-const containers = document.querySelectorAll(".container_container .container");
+const containers = document.querySelectorAll(".container_container button");
+
+containers.forEach((container) => {
+  container.addEventListener("click", () => {
+    const className = container.className;
+    const rightContainers = document.querySelectorAll('.right_container div');
+
+    // rightContainers.forEach(rightContainer => {
+    //   if (rightContainer.classList.contains(className)) {
+    //     console.log("clicked")
+    //     rightContainer.style.display = 'block';
+    //   } else {
+    //     rightContainer.style.display = 'none';
+    //   }
+
+    // });
+
+    containers.forEach((container) => {
+      container.classList.remove("active");
+    });
+
+    container.classList.add("active");
+  })
+});
 
 // PLAY-PAUSE BUTTONS
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
+pausePlayButtons.forEach((pausePlayButton) => {
+  pausePlayButton.addEventListener("click", () => {
     if (video.paused) {
       video.play();
       playButton.style.display = "none";
@@ -21,14 +44,4 @@ buttons.forEach((button) => {
 });
 
 // CONTAINER_CONTAINER
-containers.forEach((container) => {
-  container.addEventListener("click", () => {
-   
 
-    containers.forEach((container) => {
-      container.classList.remove("active");
-    });
-
-    container.classList.add("active");
-  })
-});
