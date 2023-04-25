e4e010b87be45ca01776b77b94ba48fcfa411126
jerome-ElGwapo <jeromemain07@gmail.com>
2023-04-25 22:49:13 +0800

    active state left and the rest of the stuff.

--- index.js
+++ index.js
@@ -17,6 +17,75 @@ const setVideoSize = () => {
   background.style.height = newVidHeight + 'px';
 }
 
+const openTab = (e, id) => {
+  const tabContents = document.querySelectorAll('.tab-content');
+  tabContents.forEach((tabContent) => {
+    tabContent.style.display = 'none';
+  })
+  document.querySelector(`#${id}`).style.display = 'block';
+  const tabLinks = document.querySelectorAll('.tab-link');
+  tabLinks.forEach((tabLink) => {
+    tabLink.classList.remove('.active');
+  })
+  e.currentTarget.classList.add('.active');
+}
+
+const initPage = () => {
+  let pageId = location.hash;
+
+  if (pageId) {
+    highlightMenu(document.querySelector(`.page-links[href^="${pageId}"]`));
+    showPage(document.querySelector(pageId));
+  } else {
+    pageId = document.querySelector('.page-links.active').getAttribute('href');
+    showPage(document.querySelector(pageId));
+  }
+}
+
+const highlightMenu = (menuItem) => {
+  const pageLinks = document.querySelectorAll('.page-links');
+  pageLinks.forEach((pageLink) => {
+    pageLink.classList.remove('.active');
+  })
+  menuItem.classList.add('.active');
+}
+
+const showPage = (page) => {
+  const pageContents = document.querySelectorAll('.page-contents');
+  pageContents.forEach((pageContent) => {
+    pageContent.style.display = 'none';
+  })
+  page.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initPage();
+
+  const pageLinks = document.querySelectorAll('.page-links');
+  pageLinks.forEach((pageLink) => {
+    pageLink.addEventListener('click', (event) => {
+      if (window.innerWidth > 991) {
+        event.preventDefault();
+      }
+      highlightMenu(event.currentTarget);
+      showPage(document.querySelector(event.currentTarget.hash));
+    });
+  })
+
+  // TABS >
+  const tabLinks = document.querySelectorAll('.tab-link');
+  tabLinks.forEach((tabLink) => {
+    tabLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      openTab(e, e.target.dataset.id);
+    });
+  })
+
+  document.querySelector('.tab-link .active').click();
+})
+
+
+//  VIDEO BUTTONS
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (background.paused) {
